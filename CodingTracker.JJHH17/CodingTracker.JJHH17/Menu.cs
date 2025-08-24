cf79c827b3a6bc415d08b8a79fefe495d6dfafa2
James <james.hatfield17@icloud.com>
2025-08-24 11:40:03 +0100

    Added menu class and imported menu functionality here

---
+++ CodingTracker.JJHH17/CodingTracker.JJHH17/Menu.cs
@@ -0,0 +1,46 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectre.Console;
+
+namespace CodingTracker.JJHH17
+{
+    enum MenuOptions
+    {
+        Add,
+        Exit,
+    }
+
+    internal class Menu
+    {
+        public static void Show()
+        {
+            bool active = true;
+
+            while (active)
+            {
+                Console.Clear(); // Clears the console when menu is shown 
+
+                var choice = AnsiConsole.Prompt(
+                    new SelectionPrompt<MenuOptions>()
+                    .Title("What would you like to do?")
+                    .AddChoices(Enum.GetValues<MenuOptions>()));
+
+                switch (choice)
+                {
+                    case MenuOptions.Add:
+                        AnsiConsole.MarkupLine("[green]You chose to add a new entry![/]");
+                        break;
+
+                    case MenuOptions.Exit:
+                        AnsiConsole.MarkupLine("[red]Exiting the application. Goodbye![/]");
+                        active = false;
+                        break;
+                }
+            }
+        }
+    }
+}
