70703a8c4a8b419ef30f0379d16e21d0f5806f2b
James <james.hatfield17@icloud.com>
2025-08-24 16:08:27 +0100

    Added call to create database for testing

--- CodingTracker.JJHH17/CodingTracker.JJHH17/Program.cs
+++ CodingTracker.JJHH17/CodingTracker.JJHH17/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using Spectre.Console;
+using System.IO;
 using Menu = CodingTracker.JJHH17.Menu;
+using Database = CodingTracker.JJHH17.Database;
 
 // Pulls from App.config
 // var dbConnection = ConfigurationManager.AppSettings.Get(0);
@@ -12,9 +14,10 @@ public static class Program
     {
     public static void Main()
     {
-        AnsiConsole.MarkupLine("[yellow]Welcome to Code Tracker![/]");
+        // AnsiConsole.MarkupLine("[yellow]Welcome to Code Tracker![/]");
 
-        // Main Menu
-        Menu.Show();
+        // Menu.Show();
+
+        Database.CreateDatabase();
     }
 }
