9d33c394df472e53f8f6881785541ecdd6205fcf
James <james.hatfield17@icloud.com>
2025-08-24 11:24:02 +0100

    Added main menu options (add and exit) to main

--- CodingTracker.JJHH17/CodingTracker.JJHH17/Program.cs
+++ CodingTracker.JJHH17/CodingTracker.JJHH17/Program.cs
@@ -5,10 +5,40 @@ using Spectre.Console;
 // var dbConnection = ConfigurationManager.AppSettings.Get(0);
 // Console.WriteLine(dbConnection);
 
+enum MenuOptions
+{
+    Add,
+    Exit,
+}
+
 public static class Program
 {
     public static void Main()
     {
+        bool active = true;
         AnsiConsole.MarkupLine("[yellow]Welcome to Code Tracker![/]");
+
+        // Main Menu
+        while (active)
+        {
+            Console.Clear(); // Clears the console when menu is shown 
+
+            var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<MenuOptions>()
+                .Title("What would you like to do?")
+                .AddChoices(Enum.GetValues<MenuOptions>()));
+
+            switch (choice)
+            {
+                case MenuOptions.Add:
+                    AnsiConsole.MarkupLine("[green]You chose to add a new entry![/]");
+                    break;
+
+                case MenuOptions.Exit:
+                    AnsiConsole.MarkupLine("[red]Exiting the application. Goodbye![/]");
+                    active = false;
+                    break;
+            }
+        }
     }
 }
