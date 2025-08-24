79a80974d80336dfbb949d6969c7b6dace333d6e
James <james.hatfield17@icloud.com>
2025-08-24 16:08:13 +0100

    Added method to create a database file when called

---
+++ CodingTracker.JJHH17/CodingTracker.JJHH17/Database.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using System;
+using System.IO;
+using System.Configuration;
+using System.Data.SQLite;
+
+// TODO - Add Dapper installation to README
+// TODO - Add SQLITE installation to README
+namespace CodingTracker.JJHH17
+{
+    public class Database
+    {
+        // Imports database from config file
+        private static readonly string dbPath = ConfigurationManager.AppSettings["databasePath"];
+
+        public static void CreateDatabase()
+        {
+            if (!File.Exists(dbPath))
+            {
+                SQLiteConnection.CreateFile(dbPath);
+                Console.WriteLine("Database created successfully.");
+            } else
+            {
+                Console.WriteLine("Database already exists.");
+            }
+        }
+    }
+
+}
