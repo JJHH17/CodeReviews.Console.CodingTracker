5ae9fcc85fd0e61cbe3f1ceaf7821b8742e5cfbd
James <james.hatfield17@icloud.com>
2025-08-24 16:07:30 +0100

    Added initial class structure and date string input variables

---
+++ CodingTracker.JJHH17/CodingTracker.JJHH17/CodingSession.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker.JJHH17
+{
+    internal class CodingSession
+    {
+        private string startTime;
+        private string endTime;
+
+        public CodingSession(string startTime, string endTime)
+        {
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        // To add... calculate duration
+    }
+}
