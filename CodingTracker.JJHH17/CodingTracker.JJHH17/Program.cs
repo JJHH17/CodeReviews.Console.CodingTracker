425df385d2280abefe1a541e1b4b6fd95e22064c
James <james.hatfield17@icloud.com>
2025-08-24 11:40:31 +0100

    Transferred previous menu to menu.cs, we now only call this in the main class

--- CodingTracker.JJHH17/CodingTracker.JJHH17/Program.cs
+++ CodingTracker.JJHH17/CodingTracker.JJHH17/Program.cs
@@ -1,44 +1,20 @@
 ﻿using System.Configuration;
 using Spectre.Console;
+using Menu = CodingTracker.JJHH17.Menu;
 
 // Pulls from App.config
 // var dbConnection = ConfigurationManager.AppSettings.Get(0);
 // Console.WriteLine(dbConnection);
 
-enum MenuOptions
-{
-    Add,
-    Exit,
-}
+namespace CodingTracker.JJHH17;
 
 public static class Program
-{
+    {
     public static void Main()
     {
-        bool active = true;
         AnsiConsole.MarkupLine("[yellow]Welcome to Code Tracker![/]");
 
         // Main Menu
-        while (active)
-        {
-            Console.Clear(); // Clears the console when menu is shown 
-
-            var choice = AnsiConsole.Prompt(
-                new SelectionPrompt<MenuOptions>()
-                .Title("What would you like to do?")
-                .AddChoices(Enum.GetValues<MenuOptions>()));
-
-            switch (choice)
-            {
-                case MenuOptions.Add:
-                    AnsiConsole.MarkupLine("[green]You chose to add a new entry![/]");
-                    break;
-
-                case MenuOptions.Exit:
-                    AnsiConsole.MarkupLine("[red]Exiting the application. Goodbye![/]");
-                    active = false;
-                    break;
-            }
-        }
+        Menu.Show();
     }
 }
